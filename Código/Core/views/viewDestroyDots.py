5ad4ae4a65144dbd92ca73177a0bd49ae54d69c3
CarloRob22 <roberto.cduran@yahoo.com>
2021-08-01 01:31:28 -0600

    se agrego cronometro y menú de botones al Destroy

--- Código/Core/views/viewDestroyDots.py
+++ Código/Core/views/viewDestroyDots.py
@@ -1,6 +1,8 @@
 from guizero import *
 from view import View
 from random import randint
+import time
+import json
 
 class ViewDestroyDots(View):   
     def __init__(self, title="Destroy de dots", width=840, height=620, layout="auto", bg="white", visible=True):
@@ -8,11 +10,29 @@ class ViewDestroyDots(View):
 
         self.GRID_SIZE = 5
         self.score = 0
-        self.instructions = Text(self.app, text="Click the dots to destroy them")
-        self.board = Waffle(self.app, width=self.GRID_SIZE, height=self.GRID_SIZE, command=self.destroy_dot, dim = 80)
-        self.board.after(1000, self.add_dot)
-        self.score_display = Text(self.app, text="Your score is " + str(self.score))
+        self.startTim = {}
+        self.second = 0
+        self.minutes = 0
+        self.hours = 0
+        self.aux_hour =0 
+        self.aux_min = 0
+        self.aux_sec = 0
         
+        self.BoxWaff = Box(self.app, layout="auto", align="left")
+        self.instructions = Text(self.BoxWaff, text="Click the dots to destroy them")
+        self.board = Waffle(self.BoxWaff, width=self.GRID_SIZE, height=self.GRID_SIZE, command=self.destroy_dot, dim = 80)                
+        self.board.after(1000, self.add_dot)
+        self.score_display = Text(self.BoxWaff, text="Your score is " + str(self.score))
+        
+        self.BoxBotton = Box(self.app, layout="auto", align="left", border=True, width="fill", height="fill")
+        self.BoxBottonL =Box(self.BoxBotton, align="left", width="fill", height="fill")
+        self.pauseGame = PushButton(self.BoxBottonL, text="Pause", command=self.pause, width="fill", height="fill")                       
+        self.Defeat = PushButton(self.BoxBottonL, text="Declare Defeat", command=self.declareDefeat, width="fill", height="fill")
+        self.BoxBottonR =Box(self.BoxBotton, align="left", width="fill", height="fill")    
+        self.timer = Text(self.BoxBottonR, text="")
+        
+        self.startTime()
+        self.gameTime()
         self.app.display()
 
     def add_dot(self):
@@ -53,6 +73,50 @@ class ViewDestroyDots(View):
             self.board.set_pixel(x, y, "white")
             self.score += 1
             self.score_display.value = "Your score is " + str(self.score)
+            
+    #mediante la siguiente función se obtiene la hora de inicio del juego en formato HH:MM:SS.  
+    def startTime(self):
+        hour = time.strftime("%H")
+        min = time.strftime("%M")
+        sec = time.strftime("%S")
+        
+        self.startTim = {
+            "hour":hour,
+            "min":min,
+            "sec":sec
+        }  
+        print(self.startTim["sec"])  
+    
+    
+    #Mediante la siguiente función se inicia el tiempo del juego desde (00:00:00).                
+    
+    def gameTime(self,):        
+        sec = time.strftime("%S")
+        self.second = self.second + (int(sec)-(int(sec))) +1    
+        self.aux_sec = self.second
+        
+        if self.aux_sec == 60:
+            self.aux_sec = "0"
+            self.aux_min = self.aux_min + 1
+            self.second = 0
+            
+        if self.aux_min == 60:
+            self.aux_min = "0"
+            self.aux_hour= self.aux_hour + 1
+            self.min = 0
+        
+        self.timer.tk.config(text="{}:{}:{}".format(str(self.aux_hour).zfill(2),str(self.aux_min).zfill(2),str(self.aux_sec).zfill(2)))
+               
+        self.timer.tk.after(1000, self.gameTime)                 
+                    
+               
+    #Mediante la siguiente función se pausa el tiempo del juego. La función es llamada desde el botón "pause"
+    def pause(self):
+        pass    
+    
+    #Mediante la siguiente función se declara el juego en estado de derrota. La función es llamada desde el botón "Declare Defeat" 
+    def declareDefeat(self):
+        pass
 
 #instancia para hacer pruebas
-#p = ViewDestroyDots()
+p = ViewDestroyDots()
