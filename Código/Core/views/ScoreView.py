816fd3af145974527adf86c8ec4a795a22c5a8b3
Marco Ruiz <martulioruiz@gmail.com>
2021-08-13 11:27:29 -0600

    Actualizacion error score

--- Código/Core/views/ScoreView.py
+++ Código/Core/views/ScoreView.py
@@ -13,7 +13,7 @@ class ScoreView(View):
         allBox.tk.pack_propagate(0)
 
         #INICIO SECCION DEL TITULO        
-        allColBox1 = Box(allBox, layout="auto", border=1)
+        allColBox1 = Box(allBox, layout="auto")
         allColBox1.tk.pack()
         allColBox1.tk.pack_propagate(0)
         allColBox1.resize(allBox.tk.winfo_reqwidth(), (allBox.tk.winfo_reqheight()*15)/100)
@@ -23,17 +23,17 @@ class ScoreView(View):
         #FINAL SECCION DEL TITULO
 
         #INICIO SECCION DE LA TABLA
-        allColBox2 = Box(allBox, layout="auto", border=1)
+        allColBox2 = Box(allBox, layout="auto")
         allColBox2.tk.pack_propagate(0)
         allColBox2.resize(allBox.tk.winfo_reqwidth(), (allBox.tk.winfo_reqheight()*70)/100)
         #allColBox2.bg = "beige"
 
-        tableBox = Box(allColBox2, layout="auto")
+        tableBox = Box(allColBox2, layout="auto", border=0.2)
         tableBox.resize((allColBox2.tk.winfo_reqwidth()*85)/100, (allColBox2.tk.winfo_reqheight()*85)/100)
         tableBox.tk.place(x=allColBox2.tk.winfo_reqwidth()/2, y=allColBox2.tk.winfo_reqheight()/2, anchor="center")
         tableBox.tk.pack_propagate(0)
             #INICIO FILA 1 DE LA TABLA
-        rowTableBox1 = Box(tableBox, layout="auto", border=1)
+        rowTableBox1 = Box(tableBox, layout="auto")
         rowTableBox1.resize(tableBox.tk.winfo_reqwidth(), (tableBox.tk.winfo_reqheight()*10)/100)
         rowTableBox1.tk.pack_propagate(0)
         rowTableBox1.bg ="RoyalBlue4"
@@ -81,57 +81,58 @@ class ScoreView(View):
             
                
         scores = self.gEngine.getUserScores()
-        print(scores)
+        
+        if len(scores)>0:
 
-        counter = 1
+            counter = 1
 
-        for score in scores.values():
-                #INICIO FILA 1 DE LA TABLA
-            rowTableBox1 = Box(tableBox, layout="auto", border=1)
-            rowTableBox1.resize(tableBox.tk.winfo_reqwidth(), (tableBox.tk.winfo_reqheight()*9)/100)
-            rowTableBox1.tk.pack_propagate(0)
+            for score in scores.values():
+                    #INICIO FILA 1 DE LA TABLA
+                rowTableBox1 = Box(tableBox, layout="auto")
+                rowTableBox1.resize(tableBox.tk.winfo_reqwidth(), (tableBox.tk.winfo_reqheight()*9)/100)
+                rowTableBox1.tk.pack_propagate(0)
 
-                    #INICIO COLUMNA 1 FILA 1 DE LA TABLA
-            col1RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
-            col1RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*5)/100, rowTableBox1.tk.winfo_reqheight())
-            col1RowTableBox1.tk.pack_propagate(0)
-            textCol1RowTableBox1le = Text(col1RowTableBox1, text="{}".format(counter), size=14)
-            textCol1RowTableBox1le.tk.place(x=col1RowTableBox1.tk.winfo_reqwidth()/2, y=col1RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
-                    #FINAL COLUMNA 1 FILA 1 DE LA TABLA
+                        #INICIO COLUMNA 1 FILA 1 DE LA TABLA
+                col1RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
+                col1RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*5)/100, rowTableBox1.tk.winfo_reqheight())
+                col1RowTableBox1.tk.pack_propagate(0)
+                textCol1RowTableBox1le = Text(col1RowTableBox1, text="{}".format(counter), size=14)
+                textCol1RowTableBox1le.tk.place(x=col1RowTableBox1.tk.winfo_reqwidth()/2, y=col1RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
+                        #FINAL COLUMNA 1 FILA 1 DE LA TABLA
 
-                    #INICIO COLUMNA 2 FILA 1 DE LA TABLA
-            col2RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
-            col2RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
-            col2RowTableBox1.tk.pack_propagate(0)
-            textCol2RowTableBox1le = Text(col2RowTableBox1, text="{}".format(score['gameName']), size=14)
-            textCol2RowTableBox1le.tk.place(x=col2RowTableBox1.tk.winfo_reqwidth()/2, y=col2RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
-                    #FINAL COLUMNA 2 FILA 1 DE LA TABLA
+                        #INICIO COLUMNA 2 FILA 1 DE LA TABLA
+                col2RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
+                col2RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
+                col2RowTableBox1.tk.pack_propagate(0)
+                textCol2RowTableBox1le = Text(col2RowTableBox1, text="{}".format(score['gameName']), size=14)
+                textCol2RowTableBox1le.tk.place(x=col2RowTableBox1.tk.winfo_reqwidth()/2, y=col2RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
+                        #FINAL COLUMNA 2 FILA 1 DE LA TABLA
 
-                    #INICIO COLUMNA 3 FILA 1 DE LA TABLA
-            col3RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
-            col3RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
-            col3RowTableBox1.tk.pack_propagate(0)
-            textCol3RowTableBox1le = Text(col3RowTableBox1, text="{}".format(score['time']), size=14)
-            textCol3RowTableBox1le.tk.place(x=col3RowTableBox1.tk.winfo_reqwidth()/2, y=col3RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
-                    #FINAL COLUMNA 3 FILA 1 DE LA TABLA
+                        #INICIO COLUMNA 3 FILA 1 DE LA TABLA
+                col3RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
+                col3RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
+                col3RowTableBox1.tk.pack_propagate(0)
+                textCol3RowTableBox1le = Text(col3RowTableBox1, text="{}".format(score['time']), size=14)
+                textCol3RowTableBox1le.tk.place(x=col3RowTableBox1.tk.winfo_reqwidth()/2, y=col3RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
+                        #FINAL COLUMNA 3 FILA 1 DE LA TABLA
 
-                    #INICIO COLUMNA 4 FILA 1 DE LA TABLA
-            col4RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
-            col4RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
-            col4RowTableBox1.tk.pack_propagate(0)
-            textCol4RowTableBox1le = Text(col4RowTableBox1, text="{}".format(score['score']), size=14)
-            textCol4RowTableBox1le.tk.place(x=col4RowTableBox1.tk.winfo_reqwidth()/2, y=col4RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
-                    #FINAL COLUMNA 4 FILA 1 DE LA TABLA
+                        #INICIO COLUMNA 4 FILA 1 DE LA TABLA
+                col4RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
+                col4RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
+                col4RowTableBox1.tk.pack_propagate(0)
+                textCol4RowTableBox1le = Text(col4RowTableBox1, text="{}".format(score['score']), size=14)
+                textCol4RowTableBox1le.tk.place(x=col4RowTableBox1.tk.winfo_reqwidth()/2, y=col4RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
+                        #FINAL COLUMNA 4 FILA 1 DE LA TABLA
 
-                    #INICIO COLUMNA 5 FILA 1 DE LA TABLA
-            col5RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
-            col5RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
-            col5RowTableBox1.tk.pack_propagate(0)
-            textCol5RowTableBox1le = Text(col5RowTableBox1, text="{}".format(score['date']), size=14)
-            textCol5RowTableBox1le.tk.place(x=col5RowTableBox1.tk.winfo_reqwidth()/2, y=col5RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
-                    #FINAL COLUMNA 6 FILA 1 DE LA TABLA
-            counter = counter + 1
-                #FINAL FILA 1 DE LA TABLA
+                        #INICIO COLUMNA 5 FILA 1 DE LA TABLA
+                col5RowTableBox1 = Box(rowTableBox1, layout="auto", border=1, align="left")
+                col5RowTableBox1.resize((rowTableBox1.tk.winfo_reqwidth()*23.75)/100, rowTableBox1.tk.winfo_reqheight())
+                col5RowTableBox1.tk.pack_propagate(0)
+                textCol5RowTableBox1le = Text(col5RowTableBox1, text="{}".format(score['date']), size=14)
+                textCol5RowTableBox1le.tk.place(x=col5RowTableBox1.tk.winfo_reqwidth()/2, y=col5RowTableBox1.tk.winfo_reqheight()/2, anchor="center")
+                        #FINAL COLUMNA 6 FILA 1 DE LA TABLA
+                counter = counter + 1
+                    #FINAL FILA 1 DE LA TABLA
 
 
         
